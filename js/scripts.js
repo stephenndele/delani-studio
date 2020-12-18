8a6734b20c397c5380d703851fa88de91e620065
Stephen Ndele <stephenndele09346@gmail.com>
2020-12-18 21:13:56 +0300

    Added javascript file with toggle effects

---
+++ js/scripts.js
@@ -0,0 +1,11 @@
+$(document).ready(function() {
+  $(".show").click(function() {
+    $(".card-hide").toggle();
+  });
+  $(".show1").click(function() {
+    $(".card-hide1").toggle();
+  });
+  $(".show2").click(function() {
+    $(".card-hide2").toggle();
+  });
+  });
