558e0a3bc4d2de1d3f80cd3a6d1c1632342bcfa2
Stephen Ndele <stephenndele09346@gmail.com>
2020-12-22 12:14:02 +0300

    Add mouse hover effects function

--- js/scripts.js
+++ js/scripts.js
@@ -8,4 +8,78 @@ $(document).ready(function() {
   $(".show2").click(function() {
     $(".card-hide2").toggle();
   });
+
+
+  // $(".work1").mouseover(function(){
+  //   $(".work1a").show();
+
+  // }).mouseout(function(){
+  //   $(".work1a").hide();
+  // })
+
+  $(function(){
+    $(".work1").mouseover(function(){
+      $(".work1a").show();
+    }).mouseleave(function() {
+      $(".work1a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work2").mouseover(function(){
+      $(".work2a").show();
+    }).mouseleave(function() {
+      $(".work2a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work3").mouseover(function(){
+      $(".work3a").show();
+    }).mouseleave(function() {
+      $(".work3a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work4").mouseover(function(){
+      $(".work4a").show();
+    }).mouseleave(function() {
+      $(".work4a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work5").mouseover(function(){
+      $(".work5a").show();
+    }).mouseleave(function() {
+      $(".work5a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work6").mouseover(function(){
+      $(".work6a").show();
+    }).mouseleave(function() {
+      $(".work6a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work7").mouseover(function(){
+      $(".work7a").show();
+    }).mouseleave(function() {
+      $(".work7a").hide();
+    })
+  })
+
+  $(function(){
+    $(".work8").mouseover(function(){
+      $(".work8a").show();
+    }).mouseleave(function() {
+      $(".work8a").hide();
+    })
+  })
+
   });
+
