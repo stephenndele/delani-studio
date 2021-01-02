d8ef62eb1db90b4637ce1c6425ffca942518872c
Stephen Ndele <stephenndele09346@gmail.com>
2021-01-02 10:12:05 +0300

    changed variables

--- js/scripts.js
+++ js/scripts.js
@@ -93,10 +93,10 @@ $(document).ready(function() {
 
   $("form#contact-form").on("submit", function(event) {
     event.preventDefault();
-    var name = $("input#form_name").val();
-    var enail= $("input#form_email").val();
-    var message = $("input#form_message").val();
-    var result = ("Hello " + name + " we received your message. Thank you for reaching to us well get back soon");
+    let name = $("input#form_name").val();
+    let email= $("input#form_email").val();
+    let message = $("input#form_message").val();
+    let result = ("Hello " + name + " we received your message. Thank you for reaching to us well get back soon");
 
     $("#output").text(result);
 
